f7848e003d60fe379c9ec71a6661054c376498ca
Florian Thevissen <mail@florian-thevissen.de>
2019-03-03 11:43:51 +0100

    [zsh]:  zle fzf-history-widget from fzf source

--- zsh/.zsh/06-keybindings.zsh
+++ zsh/.zsh/06-keybindings.zsh
@@ -118,6 +118,23 @@ bindkey -M menuselect 'u' undo
 
 bindkey '^[f' fzf-Mf
 bindkey -M vicmd '^[f' fzf-Mf
+
+fzf-history-widget() {
+	local selected num
+	setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
+	selected=( $(fc -rl 1 |
+					 FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+	local ret=$?
+	if [ -n "$selected" ]; then
+		num=$selected[1]
+		if [ -n "$num" ]; then
+			zle vi-fetch-history -n $num
+		fi
+	fi
+	zle reset-prompt
+	return $ret
+}
+zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
 bindkey -M vicmd '^R' fzf-history-widget
 
